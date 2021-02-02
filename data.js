e09fa91bafb37de69374c719fce09dabd67308be
Tyler Farris <Tylerpfarris@gmail.com>
2021-02-02 09:08:04 -0800

    data almost done

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ export default [
                 {
                     'id':'4',
                     'name':'Potatoes',
-                    'preparation':'Preheat oven to 375°F, cut potatoes into inch-ish chunks, toss with oil to coat and salt to taste, arrange potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
+                    'preparation':'Preheat oven to 375°F, cut potatoes into bite size chunks, toss with oil to coat and salt to taste, arrange potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
                     'preparation-shorthand':'roasted',
                     'temp':'375°F',
                     'time':'35-45 min.',
@@ -47,7 +47,7 @@ export default [
                 {
                     'id':'5',
                     'name':'Beet',
-                    'preparation':'Preheat oven to 400°F, wash and scrub beets well, cut off the leafy tops and wrap each beet in aluminum foil, place the beets in a backing dish and depending on the size roast for an hour to an hour and 45 minutes or until done, let cool for 10-15 minutes, then using a towel',
+                    'preparation':'Preheat oven to 400°F, wash and scrub beets well, cut off the leafy tops and wrap each beet in aluminum foil, place the beets in a backing dish and depending on the size roast for an hour to an hour and 45 minutes or until done, let cool for 10-15 minutes, then wipe off the skin using paper towels. Cut beets into bite size pieces, drizzle with olive oil and salt to taste.',
                     'preparation-shorthand':'roasted',
                     'temp':'400°F',
                     'time':'1-1.75 hrs.',
@@ -57,14 +57,156 @@ export default [
                 {
                     'id':'6',
                     'name':'Sweet Potatoes',
-                    'preparation':'Preheat oven to 375°F, cut potatoes into inch-ish chunks, toss with oil to coat and salt to taste, arrange potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
+                    'preparation':'Preheat oven to 425°F, cut sweet potatoes into bite size chunks, toss with oil to coat and salt to taste, arrange sweet potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
                     'preparation-shorthand':'roasted',
-                    'image':'./assets/potatoes.jpg',
-                    'image2':'./assets/roastedPotatoes.jpg'
+                    'temp':'425°F',
+                    'time':'35-45 min.',
+                    'image':'./assets/sweetPotatoes.jpg',
+                    'image2':'./assets/roastedSweetPotatoes.jpg'
                 },
-
-                
+                {
+                    'id':'7',
+                    'name':'Onions',
+                    'preparation':'Preheat oven to 400°F, cut onions into bite size pieces, toss with oil to coat and salt to taste, arrange onions in a single layer on a oiled or parchment lined backing sheet, roast for 25-30 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'400°F',
+                    'time':'25-30 min.',
+                    'image':'./assets/redAndWhiteOnions.jpg',
+                    'image2':'./assets/roastedOnions.jpg'
+                },
+                {
+                    'id':'8',
+                    'name':'Zucchini',
+                    'preparation':'Preheat oven to 425°F, cut zucchini into 1/2in thick half moons, toss with oil to coat and salt to taste, arrange zucchini in a single layer on a oiled or parchment lined backing sheet, roast for 10-15 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'425°F',
+                    'time':'10-15 min.',
+                    'image':'./assets/zucchini.jpg',
+                    'image2':'./assets/roastedZucchini.jpg'
+                },
+                {
+                    'id':'9',
+                    'name':'Corn',
+                    'preparation':'Preheat oven to 400°F, shuck the corn and remove as much of the silk as possible, toss with oil to coat and salt to taste, arrange the cobs on an aluminum lined baking dish, roast for 35-45 minutes or until done, finish under the broiler for a nice char. Cut the kernels of the cob over a large bowl to a void mess.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'400°F',
+                    'time':'35-45 min.',
+                    'image':'./assets/corn.jpg',
+                    'image2':'./assets/roastedCorn.jpg'
+                }   
             ]
-        }
+        },
+        { 'name':'soft-veg',
+            'value':[
+                { 'id':'10',
+                    'name':'Onion',
+                    'preparation':'Finely slice two onions in 1/8 strips. Place onions in a medium heatproof bowl. Bring 1 cup of vinegar, 2 tablespoons of sugar, and 1 tablespoon of salt to a boil in a saucepan; pour over onions. Let cool completely. Refrigerate, covered, for at least 2 hours.',
+                    'preparation-shorthand':'pickled',
+                    'image':'./assets/redAndWhiteOnions.jpg',
+                    'image2':'./assets/pickledRedOnions.jpg'
+                },
+                { 'id':'11',
+                    'name':'Celery',
+                    'preparation':'Slice the celery on a bias into 1/4 in strips.',
+                    'preparation-shorthand':'sliced',
+                    'image':'./assets/celery.jpg',
+                    'image2':'./assets/slicedCelery.jpg'
+                },
+                { 'id':'12',
+                    'name':'Spinach',
+                    'preparation':'Wash spinach and de-stem if necessary.',
+                    'preparation-shorthand':'raw',
+                    'image':'./assets/spinach.jpg',
+                },
+                { 'id':'13',
+                    'name':'Kale',
+                    'preparation':'Bring a pot of salted water to a rolling boil, while waiting, prepare an ice bath. Wash kale, trim the kale stems and break down the kale into bite sized pieces. Blanch the kale for 2 minutes, then shock the kale in the ice bath for 1 minute. Remove from the ice bath and pat dry with paper towels.',
+                    'preparation-shorthand':'blanched',
+                    'image':'./assets/kale.png.',
+                    
+                },
+                { 'id':'14',
+                    'name':'Bell Pepper',
+                    'preparation':'Wash, slice, and de-seed bell peppers.',
+                    'preparation-shorthand':'sliced',
+                    'image':'./assets/bellPeppers.jpg',
+                    
+                },
+                { 'id':'15',
+                    'name':'Cucumber',
+                    'preparation':'Wash, and peel if desired, slice into half moons.',
+                    'preparation-shorthand':'sliced',
+                    'image':'./assets/cucumber.jpg',
+                    
+                },
+                { 'id':'16',
+                    'name':'Green Beans',
+                    'preparation':'Bring a pot of salted water to a rolling boil, while waiting, prepare an ice bath. Wash green beans, and trim the stems. Blanch the green beans for 2 minutes, then shock the green beans in the ice bath for 1 minute. Remove from the ice bath and pat dry with paper towels.',
+                    'preparation-shorthand':'blanched',
+                    'image':'./assets/greenBeans.jpg',
+                    
+                },
+                { 'id':'17',
+                    'name':'Tomatoes',
+                    'preparation':'Wash and de-stem tomatoes',
+                    'preparation-shorthand':'raw',
+                    'image':'./assets/tomatoes.jpg',
+                    
+                },
+                { 'id':'18',
+                    'name':'Mushrooms',
+                    'preparation':'Slice mushrooms into 1/4 in pieces, depending on the type and size you may also need to cut them in half. Bring a large saute pan to temp over medium-high heat. Add just enough oil to coat the bottom of the pan. Saute the mushrooms for 4-5 minutes stirring occasionally, add a couple glugs of vegetable or chicken stock to the pan and cover with a lid. Reduce the heat to medium low and cook for another 4 minutes stirring occasionally, season to taste.',
+                    'preparation-shorthand':'sauted',
+                    'image':'./assets/mushrooms.jpg',
+                    'image2':'assets/sautedMushrooms.jpg'
+                    
+                },
+                { 'id':'19',
+                    'name':'Lettuce',
+                    'preparation':'Wash and trim lettuce',
+                    'preparation-shorthand':'raw',
+                    'image':'./assets/lettuce.jpg'
+                    
+                }
+            ]
+        },
+        {'name':'Legumes',
+            'value':[
+                { 'id':'20',
+                    'name':'Chickpeas',
+                    'preparation':'Wash and sort the chickpeas to make sure there are no stones or debris. Cover the chickpeas in cold water and soak over night or for at least 12 hours. Drain and wash the chickpeas and transfer them to a pot- cover with cold water twice the amount of chickpeas and bring to a boil. Cover, lower the heat and let simmer for 60 minutes or until they are done to your liking. Drain and let cool for at least 15 minutes.',
+                    'preparation-shorthand':'boiled',
+                    'image':'./assets/chickpeas.jpg'
+                },
+                { 'id':'21',
+                    'name':'Green or Brown Lentils',
+                    'preparation':'Wash and sort the lentils to make sure there are no stones or debris. Transfer them to a pot- cover with stock or water twice the amount of the lentils and bring to a boil. Cover, lower the heat and let simmer for 25-30 minutes or until they are done to your liking. Drain and season with salt and pepper to taste.',
+                    'preparation-shorthand':'boiled',
+                    'image':'./assets/greenLentils.jpg'
+                },
+                { 'id':'22',
+                    'name':'Black Beans',
+                    'preparation':'Wash and sort the black beans to make sure there are no stones or debris. Cover the black beans in cold water and soak over night or at least 12 hours. Drain and wash the black beans and transfer them to a pot- cover with cold  water twice the amount of the black beans and bring to a boil. Cover, lower the heat and let simmer for around 2 hours stirring periodically.You may need to top off the pot with boiling water due to evaporation. Drain and season with salt and pepper to taste.',
+                    'preparation-shorthand':'boiled',
+                    'image':'./assets/blackBeans.jpg'
+                }
+            ]
+        },
+        {'name':'Starch',
+            'value':[
+                { 'id':'23',
+                    'name':'White Rice',
+                    'preparation':'Wash the rice three times. if you have a rice cooker (you should) put the rice in it, if not place it in a sauce pan and cover with equal parts water. bring to a boil, stir, reduce to a low simmer and cover with a lid for 15 minutes before checking.',
+                    'preparation-shorthand':'boiled',
+                    'image':'./assets/rice.jpg'
+                },  
+                { 'id':'24',
+                    'name':'White Rice',
+                    'preparation':'Wash the rice three times. if you have a rice cooker (you should) put the rice in it, if not place it in a sauce pan and cover with equal parts water. bring to a boil, stir, reduce to a low simmer and cover with a lid for 15 minutes before checking.',
+                    'preparation-shorthand':'boiled',
+                    'image':'./assets/rice.jpg'
+                },  
+                
+         }
     ]
-];
+]
