40e2999cd22a6484ac915a3f2435db22c6fb279d
Tyler Farris <Tylerpfarris@gmail.com>
2021-02-01 18:02:10 -0800

    start of data entry

---
+++ data.js
@@ -0,0 +1,70 @@
+
+
+export default [
+    [
+        { 'name':'hard-veg',
+            'value':[
+                { 'id':'1',
+                    'name':'Broccoli',
+                    'preparation':'Preheat oven to 425°F, toss cut broccoli with oil to coat and salt to taste, arrange florets in a single layer on a oiled or parchment lined backing sheet, roast for 16-20 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'425°F',
+                    'time':'16-20 min.',
+                    'image':'./assets/broccoli.jpg',
+                    'image2':'./assets/roastedBroccoli.jpg'
+                },
+                {
+                    'id':'2',
+                    'name':'Cauliflower',
+                    'preparation':'Preheat oven to 425°F, toss cut cauliflower with oil to coat and salt to taste, arrange florets in a single layer on a oiled or parchment lined backing sheet, roast for 40-45 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'425°F',
+                    'time':'40-45 min.',
+                    'image':'./assets/cauliflower.jpg',
+                    'image2':'./assets/roastedCauliflower.jpg'
+
+                },
+                {
+                    'id':'3',
+                    'name':'Brussels Sprouts',
+                    'preparation':'Preheat oven to 400°F, toss cut brussels sprouts with oil to coat and salt to taste, arrange sprouts cut side down in a single layer on a oiled or parchment lined backing sheet, roast for 25 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'400°F',
+                    'time':'25 min.',
+                    'image':'./assets/brussels.jpg',
+                    'image2':'./assets/roastedBrussels.jpg'
+                },
+                {
+                    'id':'4',
+                    'name':'Potatoes',
+                    'preparation':'Preheat oven to 375°F, cut potatoes into inch-ish chunks, toss with oil to coat and salt to taste, arrange potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'temp':'375°F',
+                    'time':'35-45 min.',
+                    'image':'./assets/potatoes.jpg',
+                    'image2':'./assets/roastedPotatoes.jpg'
+                },
+                {
+                    'id':'5',
+                    'name':'Beet',
+                    'preparation':'Preheat oven to 400°F, wash and scrub beets well, cut off the leafy tops and wrap each beet in aluminum foil, place the beets in a backing dish and depending on the size roast for an hour to an hour and 45 minutes or until done, let cool for 10-15 minutes, then using a towel',
+                    'preparation-shorthand':'roasted',
+                    'temp':'400°F',
+                    'time':'1-1.75 hrs.',
+                    'image':'./assets/beet.jpg',
+                    'image2':'./assets/roastedBeets.jpg'
+                },
+                {
+                    'id':'6',
+                    'name':'Sweet Potatoes',
+                    'preparation':'Preheat oven to 375°F, cut potatoes into inch-ish chunks, toss with oil to coat and salt to taste, arrange potatoes in a single layer on a oiled or parchment lined backing sheet, roast for 35-45 minutes or until done.',
+                    'preparation-shorthand':'roasted',
+                    'image':'./assets/potatoes.jpg',
+                    'image2':'./assets/roastedPotatoes.jpg'
+                },
+
+                
+            ]
+        }
+    ]
+];
