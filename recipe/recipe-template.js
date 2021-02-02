39c8b669a58f0875bf064763b74db722417b71d8
ERICH SAUER <erich.sauer@gmail.com>
2021-02-01 17:36:18 -0800

    adds some stuff

--- recipe/recipe-template.js
+++ recipe/recipe-template.js
@@ -5,22 +5,55 @@ const lightVegPrep = 'blanched';
 const protein = 'chicken';
 const proteinPrep = 'broiled';
 const grain = 'quinoa';
-const rawVeg = 'spinach';
-const sauce = 'peanut';
+const grainPrep = 'prepared';
+const rawVeg = 'onion';
+const rawVegPrep = 'sliced';
+const sauce = 'peanut sauce';
+const acid = 'lemon juice';
+const oil = 'olive';
+const wrap = 'tortilla';
+const herb = 'basil';
+
+const affirmationList = [
+    `You got this!`,
+    `Look What's Cooking!`,
+    `You're awesome!`,
+    `Kale yeah!`,
+    `Is it hot in here?`,
+    `Fax me!`,
+    `Yummmmmmmm!`,
+    `That looks amazing!`,
+    `You did it!`,
+];
 
 const testDiv = document.getElementById('test-div');
+
+const randomAffirmation = affirmationList[Math.floor(Math.random() * affirmationList.length)];
 
 const recipes = [
     {
         id: 1,
-        title: `RobinFood Bowl`,
-        stepOne: `Toss a handful of ${rawVeg} with a sprinkle of salt, a splash of vinegar and a drizzle of finishing oil of your choice and set aside.`,
-        stepTwo: `Reheat your prepared ${grain}, ${heartyVegPrep} ${heartyVeg}, ${lightVegPrep} ${lightVeg} and ${proteinPrep} ${protein} separately, tossing one or more in ${sauce} sauce to make things more exciting.`,
-        stepThree: `Assemble your bowl by arranging each element neatly and garnish with the quick-pickled ${rawVeg}.`
+        title: `"${randomAffirmation}" Bowl`,
+        stepOne: `Toss a handful of ${rawVegPrep} ${rawVeg} with a sprinkle of salt, a splash of ${acid} and a drizzle of ${oil} oil. Set aside for now.`,
+        stepTwo: `Reheat your prepared ${grain}, ${heartyVegPrep} ${heartyVeg}, ${lightVegPrep} ${lightVeg} and ${proteinPrep} ${protein} separately, tossing one or more in ${sauce} to make things more exciting.`,
+        stepThree: `Assemble your bowl by arranging each element neatly over a base of ${grainPrep} ${grain}.`,
+        stepFour: `Garnish your RobinFood Bowl with the quick-pickled ${rawVeg} and chopped ${herb}.`
     },
     {
+        id: 2,
+        title: `"${randomAffirmation}" Wrap`,
+        stepOne: `Toss a handful of ${rawVegPrep} ${rawVeg} with a sprinkle of salt, a splash of ${acid} and a drizzle of ${oil} oil and set aside.`,
+        stepTwo: `Reheat your ${grainPrep} ${grain}, ${heartyVegPrep} ${heartyVeg}, ${lightVegPrep} ${lightVeg} and ${proteinPrep} ${protein} together, tossing in enough ${sauce} to evenly coat ingredients.`,
+        stepThree: `Assemble your wrap by positioning the filling in the lower third of a ${wrap}, folding in the left and right sides, and rolling up tightly. quick-pickled ${rawVeg}.`
+    },
+    {
+        id: 3,
+        title: `"${randomAffirmation}" Stir Fry`,
+        stepOne: `Toss a handful of ${rawVegPrep} ${rawVeg} with a sprinkle of salt, a splash of ${acid} and a drizzle of ${oil} oil and set aside.`,
+        stepTwo: `Reheat your prepared ${grain}, ${heartyVegPrep} ${heartyVeg}, ${lightVegPrep} ${lightVeg} and ${proteinPrep} ${protein} together, tossing in enough ${sauce} to evenly coat ingredients.`,
+        stepThree: `Assemble your wrap by positioning the filling in the lower third of a ${wrap}, folding in the left and right sides, and rolling up tightly. quick-pickled ${rawVeg}.`
+    },
 
-    }
 ];
 
-testDiv.textContent = `Step 1: ${recipes[0].stepOne} Step 2: ${recipes[0].stepTwo} Step 3: ${recipes[0].stepThree}`;
+testDiv.textContent = `${recipes[1].title} Step 1: ${recipes[1].stepOne} Step 2: ${recipes[1].stepTwo} Step 3: ${recipes[1].stepThree}`;
